f5f1516dd6dfdd3d3ef168ebde93c2c2f3731170
alexneumayr <alex.nmr@outlook.com>
2025-02-05 21:01:29 +0100

    Add products page

---
+++ app/products/page.js
@@ -0,0 +1,9 @@
+export const metadata = {
+  title: 'Products',
+  description:
+    'Explore the latest tech at great prices. From smartphones to accessories, we have everything you need to stay connected.',
+};
+
+export default function ProductPage() {
+  return <h1>Our products</h1>;
+}
