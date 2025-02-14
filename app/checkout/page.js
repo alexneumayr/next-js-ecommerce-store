02c900c21fe6dc7b2182d03d5bfba9d78c6f3af5
alexneumayr <alex.nmr@outlook.com>
2025-02-14 20:44:16 +0100

    Show cart products on checkout page

--- app/checkout/page.js
+++ app/checkout/page.js
@@ -1,3 +1,7 @@
+import Link from 'next/link';
+import { getProducts } from '../database/products';
+import { getCookie } from '../util/cookies';
+import { parseJson } from '../util/json';
 import CheckoutForm from './CheckoutForm';
 
 export const metadata = {
@@ -6,11 +10,65 @@ export const metadata = {
     'Secure your order with a fast and easy checkout. Enjoy great deals on the latest tech gadgets, electronics, and accessories with safe payment options and quick shipping!',
 };
 
-export default function CheckoutPage() {
+export default async function CheckoutPage() {
+  const cartCookie = await getCookie('cart');
+  const cart = parseJson(cartCookie) || [];
+  const allProducts = await getProducts();
+  const cartProducts = allProducts
+    .map((product) => {
+      const correlatingCartProduct = cart.find(
+        (item) => item.id === product.id,
+      );
+      if (correlatingCartProduct !== undefined) {
+        return {
+          id: product.id,
+          name: product.name,
+          slug: product.slug,
+          price: product.price,
+          amount: correlatingCartProduct.amount,
+          subtotal: product.price * correlatingCartProduct.amount,
+        };
+      } else {
+        return null;
+      }
+    })
+    .filter((product) => product !== null);
+  const total = cartProducts.reduce(
+    (prevValue, currentValue) => prevValue + currentValue.subtotal,
+    0,
+  );
   return (
-    <div>
+    <>
       <h1>Checkout</h1>
       <CheckoutForm />
-    </div>
+      {cartProducts.map((product) => {
+        return (
+          <div
+            data-test-id={`cart-product-${product.slug}`}
+            key={`product-${product.slug}`}
+          >
+            <br />
+            Id: {product.id}
+            <br />
+            Name:
+            <Link href={`/products/${product.slug}`}>{product.name}</Link>
+            <br />
+            Price: {(product.price / 100).toFixed(2)}
+            <br />
+            Amount:
+            <span data-test-id={`cart-product-quantity-${product.slug}`}>
+              {product.amount}
+            </span>
+            <br />
+            Subtotal: {(product.subtotal / 100).toFixed(2)}
+            <br />
+          </div>
+        );
+      })}
+      <div>
+        <br />
+        Total: {(total / 100).toFixed(2)}
+      </div>
+    </>
   );
 }
