71b70f4d9f36c485cea1d876c45396ed7bef9972
alexneumayr <alex.nmr@outlook.com>
2025-02-20 01:05:00 +0100

    Add migrations for users table

---
+++ migrations/00002-createTableUsers.ts
@@ -0,0 +1,15 @@
+import type { Sql } from 'postgres';
+
+export async function up(sql: Sql) {
+  await sql`
+    CREATE TABLE users (
+      id integer PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+      username varchar(150) NOT NULL UNIQUE,
+      password varchar(150) NOT NULL,
+    )
+  `;
+}
+
+export async function down(sql: Sql) {
+  await sql`DROP TABLE users`;
+}
---
+++ migrations/00003-insertUsers.ts
@@ -0,0 +1,41 @@
+import type { Sql } from 'postgres';
+
+const users = [
+  {
+    id: 1,
+    username: 'alex',
+    password: '$2b$10$rDMMNZjqxD8LmQfm/uPxzuuFFn8nttIH.5pwdgwq6cg7GB29anmxi',
+    role: 'user',
+  },
+  {
+    id: 2,
+    username: 'admin',
+    password: '$2b$10$a.RmIfUfl8QPS1titQUcIu0N.vSDIgBT6f408ku0kBLP6LE5reBE.',
+    role: 'admin',
+  },
+];
+
+export async function up(sql: Sql) {
+  for (const user of users) {
+    await sql`
+      INSERT INTO
+        users (username, password, role,)
+      VALUES
+        (
+          ${user.username},
+          ${user.password},
+          ${user.role},
+        )
+    `;
+  }
+}
+
+export async function down(sql: Sql) {
+  for (const user of users) {
+    await sql`
+      DELETE FROM users
+      WHERE
+        id = ${user.id}
+    `;
+  }
+}
