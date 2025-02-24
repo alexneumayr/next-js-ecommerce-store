25dfa8df7c49d7d1d514a854d0ef07022bc317d6
alexneumayr <alex.nmr@outlook.com>
2025-02-24 19:24:48 +0100

    Style logout

--- app/logout/LogoutMain.js
+++ app/logout/LogoutMain.js
@@ -13,9 +13,11 @@ export default function LogoutMain() {
   }, []);
 
   return (
-    <>
-      <h1>Logging out</h1>
-      <p>You will be redirected to the main page shortly...</p>
-    </>
+    <div className="h-[65vh] flex  flex-col justify-center">
+      <h1 className="text-6xl font-extrabold text-center mb-10">Logging out</h1>
+      <p className=" text-black text-[25px] font-normal text-center">
+        You will be redirected to the main page shortly...
+      </p>
+    </div>
   );
 }
