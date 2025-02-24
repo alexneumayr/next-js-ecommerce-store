7273cc20b65bf90a554453ddf522c9f2ebd1eeb0
alexneumayr <alex.nmr@outlook.com>
2025-02-24 17:24:14 +0100

    Style thank you page

--- app/thankyou/page.js
+++ app/thankyou/page.js
@@ -1,3 +1,5 @@
+import Link from 'next/link';
+
 export const metadata = {
   title: 'Thank you for your order',
   description:
@@ -5,5 +7,36 @@ export const metadata = {
 };
 
 export default function ThankYouPage() {
-  return <h1>Thank You for Your Purchase!</h1>;
+  return (
+    <div className="mx-[80px]">
+      <h1 className="mt-5 text-6xl font-extrabold text-center">
+        Thank You for Your Purchase!
+      </h1>
+      <div className="m-10 max-w-[1000px] rounded-[25px] border border-[#878787] mx-auto p-5">
+        <p className="text-[21px] font-normal m-3 ">
+          Your order has been successfully placed!
+          <br /> We appreciate your trust in us and are excited to get your new
+          tech to you as soon as possible.
+        </p>
+        <h2 className="m-3 text-[21px] font-semibold">What's next?</h2>
+        <ul className="list-inside list-['👉'] m-3 text-[21px] font-normal">
+          <li>
+            You'll receive a confirmation email shortly with your order details.
+          </li>
+          <li>
+            Once your package ships, we'll send you a tracking link so you can
+            follow your delivery.
+          </li>
+        </ul>
+        <h2 className="m-3 text-[21px] font-semibold">Need Help?</h2>
+        <p className="m-3 text-[21px] font-normal">
+          If you have any questions or need assistance, our support team is here
+          for you:&nbsp;
+          <Link className="text-primary underline hover:text-black" href="/">
+            Contact Support
+          </Link>
+        </p>
+      </div>
+    </div>
+  );
 }
